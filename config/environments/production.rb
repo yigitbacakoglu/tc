1c72ed8586249f112998630dbffd89ebcee758ff
yigitcan bacakoglu <yigitcan_bacakoglu@hotmail.com>
2013-05-05 19:36:30 +0300

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Omrats::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.action_mailer.default_url_options = { :host => 'talkycloud.com' }
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
